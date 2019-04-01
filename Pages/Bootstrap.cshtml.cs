74693d7597eaf0f6a19b11932bd28e74343e6431
Ryan Gross <ryangross123@icloud.com>
2019-04-01 12:01:34 -0500

    finished lab 10

--- Pages/Bootstrap.cshtml.cs
+++ Pages/Bootstrap.cshtml.cs
@@ -5,11 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Lab_10_Bootstrap.Pages
 {
     public class BootstrapModel : PageModel
     {
+        [BindProperty] 
+        [Display(Name = "First Name")]
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string FirstName {get; set;}
+
+        [BindProperty] 
+        [Display(Name = "Last Name")]
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string LastName {get; set;}
+
+        [BindProperty] 
+        [Display(Name = "Credit Card")]
+        [Required(ErrorMessage = "Required")]
+        [CreditCard]
+        public string CreditCard {get; set;}
+
         private readonly ILogger _log;
         public BootstrapModel(ILogger<BootstrapModel> log) 
         {
@@ -24,7 +44,7 @@ namespace Lab_10_Bootstrap.Pages
 
         public void OnPost()
         {
-            _log.LogInformation("OnPost()Called");
+            _log.LogWarning($"{FirstName} {LastName} {CreditCard}");
         }
     }
 }
